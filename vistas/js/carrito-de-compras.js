3c2cc22dfd4ebbf311bbae063edfc73399594366
David Boxler <davidboxler47@gmail.com>
2020-10-27 18:28:25 -0300

    CarritoParte3

--- vistas/js/carrito-de-compras.js
+++ vistas/js/carrito-de-compras.js
@@ -24,103 +24,153 @@ if(localStorage.getItem("cantidadCesta") != null){
 /*=============================================
 VISUALIZAR LOS PRODUCTOS EN LA PÁGINA CARRITO DE COMPRAS
 =============================================*/
+
 if(localStorage.getItem("listaProductos") != null){
 
 	var listaCarrito = JSON.parse(localStorage.getItem("listaProductos"));
 
-	listaCarrito.forEach(funcionForEach);
-
-	function funcionForEach(item, index){
-
-		$(".cuerpoCarrito").append(
-
-			'<div clas="row itemCarrito">'+
-
-					'<div class="col-sm-1 col-xs-12">'+
-						
-						'<br>'+
-
-						'<center>'+
-							
-							'<button class="btn btn-default backColor quitarItemCarrito" idProducto="'+item.idProducto+'" peso="'+item.peso+'">'+
-								
-								'<i class="fa fa-times"></i>'+
-
-							'</button>'+
-
-						'</center>'+
-
-					'</div>'+
-
-					'<div class="col-sm-1 col-xs-12">'+
-						
-						'<figure>'+
-							
-							'<img src="'+item.imagen+'" class="img-thumbnail">'+
-
-						'</figure>'+
-
-				'</div>'+
-
-					'<div class="col-sm-4 col-xs-12">'+
-
-						'<br>'+
-
-						'<p class="tituloCarritoCompra text-left">'+item.titulo+'</p>'+
-
-					'</div>'+
-
-					'<div class="col-md-2 col-sm-1 col-xs-12">'+
-
-						'<br>'+
-
-						'<p class="precioCarritoCompra text-center">USD $<span>'+item.precio+'</span></p>'+
-
-					'</div>'+
-
-					'<div class="col-md-2 col-sm-3 col-xs-8">'+
-
-						'<br>'+
-
-						'<div class="col-xs-8">'+
-
-							'<center>'+
-							
-								'<input type="number" class="form-control cantidadItem" min="1" value="'+item.cantidad+'" precio="'+item.precio+'" idProducto="'+item.idProducto+'">'+
-
-							'</center>'+
-
-						'</div>'+
-
-					'</div>'+
-
-					'<div class="col-md-2 col-sm-1 col-xs-4 text-center">'+
-						
-						'<br>'+
-
-						'<p class="subTotal'+item.idProducto+' subtotales">'+
-							
-							'<strong>$<span>'+item.precio+'</span></strong>'+
-
-						'</p>'+
-
-					'</div>'+
-					
-				'</div>'+
-
-				'<div class="clearfix"></div>'+
-
-				'<hr>')
-
-	}
 
 }else{
 
 	$(".cuerpoCarrito").html('<div class="well">Aún no hay productos en el carrito de compras.</div>');
 	$(".sumaCarrito").hide();
 	$(".cabeceraCheckout").hide();
-
 }
+
+for(var i = 0; i < indice.length; i++){
+
+	if(indice[i] == "carrito-de-compras"){
+
+	listaCarrito.forEach(funcionForEach);
+
+	function funcionForEach(item, index){
+
+		var datosProducto = new FormData();
+			var precio = 0;
+
+			datosProducto.append("id", item.idProducto);
+
+			$.ajax({
+
+				url:rutaOculta+"ajax/producto.ajax.php",
+				method:"POST",
+				data: datosProducto,
+				cache: false,
+				contentType: false,
+				processData:false,
+				dataType: "json",
+				success: function(respuesta){
+
+					if(respuesta["precioOferta"] == 0){
+
+						precio = respuesta["precio"];
+
+					}else{
+
+						precio = respuesta["precioOferta"];
+						
+					}
+
+					$(".cuerpoCarrito").append(
+
+						'<div clas="row itemCarrito">'+
+
+								'<div class="col-sm-1 col-xs-12">'+
+									
+									'<br>'+
+
+									'<center>'+
+										
+										'<button class="btn btn-default backColor quitarItemCarrito" idProducto="'+item.idProducto+'" peso="'+item.peso+'">'+
+											
+											'<i class="fa fa-times"></i>'+
+
+										'</button>'+
+
+									'</center>'+
+
+								'</div>'+
+
+								'<div class="col-sm-1 col-xs-12">'+
+									
+									'<figure>'+
+										
+										'<img src="'+item.imagen+'" class="img-thumbnail">'+
+
+									'</figure>'+
+
+							'</div>'+
+
+								'<div class="col-sm-4 col-xs-12">'+
+
+									'<br>'+
+
+									'<p class="tituloCarritoCompra text-left">'+item.titulo+'</p>'+
+
+								'</div>'+
+
+								'<div class="col-md-2 col-sm-1 col-xs-12">'+
+
+									'<br>'+
+
+									'<p class="precioCarritoCompra text-center"> $<span>'+precio+'</span></p>'+
+
+								'</div>'+
+
+								'<div class="col-md-2 col-sm-3 col-xs-8">'+
+
+									'<br>'+
+
+									'<div class="col-xs-8">'+
+
+										'<center>'+
+										
+											'<input type="number" class="form-control cantidadItem" min="1" value="'+item.cantidad+'" precio="'+precio+'" idProducto="'+item.idProducto+'" item="'+index+'">'+
+
+										'</center>'+
+
+									'</div>'+
+
+								'</div>'+
+
+								'<div class="col-md-2 col-sm-1 col-xs-4 text-center">'+
+									
+									'<br>'+
+
+									'<p class="subTotal'+index+' subtotales">'+
+										
+										'<strong>$<span>'+(Number(item.cantidad)*Number(precio))+'</span></strong>'+
+
+									'</p>'+
+
+								'</div>'+
+								
+							'</div>'+
+
+							'<div class="clearfix"></div>'+
+
+							'<hr>')
+
+							/*=============================================
+							/*=============================================
+							/*=============================================
+							/*=============================================
+							/*=============================================
+							ACTUALIZAR SUBTOTAL
+							=============================================*/
+							var precioCarritoCompra = $(".cuerpoCarrito .precioCarritoCompra span");
+
+							cestaCarrito(precioCarritoCompra.length);
+							
+							sumaSubtotales();
+					
+				   }			
+			})				
+
+	    }
+
+    }
+}	
 
 /*=============================================
 /*=============================================
@@ -192,8 +242,29 @@ $(".agregarCarrito").click(function(){
 
 			}else{
 
+				var listaProductos = JSON.parse(localStorage.getItem("listaProductos"));
+
+				for(var i = 0; i < listaProductos.length; i++){
+
+					if(listaProductos[i]["idProducto"] == idProducto){
+
+						swal({
+						title: "El producto ya está agregado al carrito de compras",
+						text: "",
+						type: "warning",
+						showCancelButton: false,
+						confirmButtonColor: "#DD6B55",
+						confirmButtonText: "¡Volver!",
+						closeOnConfirm: false
+						})
+
+						return;
+
+					}
+
+				}
+
 				listaCarrito.concat(localStorage.getItem("listaProductos"));
-				console.log("listaCarrito", listaCarrito);
 
 			}
 
@@ -291,7 +362,8 @@ $(".quitarItemCarrito").click(function(){
 			}
 	
 			localStorage.setItem("listaProductos",JSON.stringify(listaCarrito));
-	
+			
+			sumaSubtotales();
 			cestaCarrito(listaCarrito.length);
 		
 		}else{
@@ -331,7 +403,7 @@ $(document).on("change", ".cantidadItem", function () {
 	var idProducto = $(this).attr("idProducto");
 	var item = $(this).attr("item");
    
-	$(".subTotal" + item).html('<strong>USD $<span>' + (cantidad * precio) + '</span></strong>');
+	$(".subTotal" + item).html('<strong> $<span>' + (cantidad * precio) + '</span></strong>');
    
 	/*=============================================
 	ACTUALIZAR LA CANTIDAD EN EL LOCALSTORAGE
@@ -368,30 +440,9 @@ $(document).on("change", ".cantidadItem", function () {
 	localStorage.setItem("listaProductos", JSON.stringify(listaCarrito));
    
 	sumaSubtotales();
+	cestaCarrito(listaCarrito.length);
 
 })	
-
-/*=============================================
-/*=============================================
-/*=============================================
-/*=============================================
-/*=============================================
-ACTUALIZAR SUBTOTAL
-=============================================*/
-var precioCarritoCompra = $(".cuerpoCarrito .precioCarritoCompra span");
-var cantidadItem = $(".cuerpoCarrito .cantidadItem");
-
-for(var i = 0; i < precioCarritoCompra.length; i++){
-
-	var precioCarritoCompraArray = $(precioCarritoCompra[i]).html();
-	var cantidadItemArray = $(cantidadItem[i]).val();
-	var idProductoArray = $(cantidadItem[i]).attr("idProducto");
-
-	$(".subTotal"+idProductoArray).html('<strong> $<span>'+(precioCarritoCompraArray*cantidadItemArray)+'</span></strong>');
-
-	sumaSubtotales();
-
-}
 
 /*=============================================
 /*=============================================
@@ -421,7 +472,7 @@ function sumaSubtotales(){
 
 	var sumaTotal = arraySumaSubtotales.reduce(sumaArraySubtotales);
 	
-	$(".sumaSubTotal").html('<strong>USD $<span>'+(sumaTotal).toFixed(2)+'</span></strong>');
+	$(".sumaSubTotal").html('<strong> $<span>'+(sumaTotal).toFixed(2)+'</span></strong>');
 
 	$(".sumaCesta").html((sumaTotal).toFixed(2));
 
@@ -429,5 +480,229 @@ function sumaSubtotales(){
 
 }
 
+/*=============================================
+/*=============================================
+/*=============================================
+/*=============================================
+/*=============================================
+ACTUALIZAR CESTA AL CAMBIAR CANTIDAD
+=============================================*/
+function cestaCarrito(cantidadProductos){
 
+	/*=============================================
+	SI HAY PRODUCTOS EN EL CARRITO
+	=============================================*/
+
+	if(cantidadProductos != 0){
+		
+		var cantidadItem = $(".cuerpoCarrito .cantidadItem");
+
+		var arraySumaCantidades = [];
+	
+		for(var i = 0; i < cantidadItem .length; i++){
+
+			var cantidadItemArray = $(cantidadItem[i]).val();
+			arraySumaCantidades.push(Number(cantidadItemArray));
+			
+		}
+	
+		function sumaArrayCantidades(total, numero){
+
+			return total + numero;
+
+		}
+
+		var sumaTotalCantidades = arraySumaCantidades.reduce(sumaArrayCantidades);
+		
+		$(".cantidadCesta").html(sumaTotalCantidades );
+		localStorage.setItem("cantidadCesta", sumaTotalCantidades);
+
+	}
+
+}
+
+
+/*=============================================
+/*=============================================
+/*=============================================
+/*=============================================
+/*=============================================
+CHECKOUT
+=============================================*/
+
+$("#btnCheckout").click(function(){
+
+	$(".listaProductos table.tablaProductos tbody").html("");
+
+	var idUsuario = $(this).attr("idUsuario");
+	var peso = $(".cuerpoCarrito button");
+	var titulo = $(".cuerpoCarrito .tituloCarritoCompra");
+	var cantidad = $(".cuerpoCarrito .cantidadItem");
+	var subtotal = $(".cuerpoCarrito .subtotales span");
+	var cantidadPeso = [];
+
+
+	/*=============================================
+	SUMA SUBTOTAL
+	=============================================*/
+
+	var sumaSubTotal = $(".sumaSubTotal span")
+	
+	$(".valorSubtotal").html($(sumaSubTotal).html());
+
+	/*=============================================
+	TASAS DE IMPUESTO
+	=============================================*/
+
+	var impuestoTotal = ($(".valorSubtotal").html() * $("#tasaImpuesto").val()) /100;
+	$(".valorTotalImpuesto").html(impuestoTotal.toFixed(1));
+
+	sumaTotalCompra()
+
+	/*=============================================
+	VARIABLES ARRAY 
+	=============================================*/
+
+	for(var i = 0; i < titulo.length; i++){
+
+		var pesoArray = $(peso[i]).attr("peso");
+		var tituloArray = $(titulo[i]).html();
+		var cantidadArray = $(cantidad[i]).val();		
+		var subtotalArray = $(subtotal[i]).html();
+
+		/*=============================================
+		EVALUAR EL PESO DE ACUERDO A LA CANTIDAD DE PRODUCTOS
+		=============================================*/
+
+		cantidadPeso[i] = pesoArray * cantidadArray;
+
+		function sumaArrayPeso(total, numero){
+
+			return total + numero;
+
+		}
+
+		var sumaTotalPeso = cantidadPeso.reduce(sumaArrayPeso);
+
+		/*=============================================
+		MOSTRAR PRODUCTOS DEFINITIVOS A COMPRAR
+		=============================================*/
+
+		$(".listaProductos table.tablaProductos tbody").append('<tr>'+
+															   '<td>'+tituloArray+'</td>'+
+															   '<td>'+cantidadArray+'</td>'+
+															   '<td>$<span>'+subtotalArray+'</span></td>'+
+															   '<tr>');
+
+			}	
+
+	    /*=============================================
+		SELECCIONAR PAÍS DE ENVÍO SI HAY PRODUCTOS FÍSICOS
+		=============================================*/
+
+		$(".formEnvio").show();
+
+		$.ajax({
+			url:rutaOculta+"vistas/js/plugins/countries.json",
+			type: "GET",
+			cache: false,
+			contentType: false,
+			processData:false,
+			dataType:"json",
+			success: function(respuesta){
+
+				respuesta.forEach(seleccionarPais);
+
+				function seleccionarPais(item, index){
+
+					var pais = item.name;
+					var codPais = item.code;
+
+					$("#seleccionarPais").append('<option value="'+codPais+'">'+pais+'</option>');
+				
+				}
+
+			}
+		})
+
+		$("#seleccionarPais").change(function(){
+
+			$(".alert").remove();
+
+			var pais = $(this).val();
+			var tasaPais = $("#tasaPais").val();
+
+			if(pais == tasaPais){
+
+				var resultadoPeso = sumaTotalPeso * $("#envioNacional").val();
+				
+				if(resultadoPeso < $("#tasaMinimaNal").val()){
+
+					$(".valorTotalEnvio").html($("#tasaMinimaNal").val());
+
+				}else{
+
+					$(".valorTotalEnvio").html(resultadoPeso);
+
+				}
+			}else{
+
+				var resultadoPeso = sumaTotalPeso * $("#envioInternacional").val();
+				
+				if(resultadoPeso < $("#tasaMinimaInt").val()){
+
+					$(".valorTotalEnvio").html($("#tasaMinimaInt").val());
+
+				}else{
+
+					$(".valorTotalEnvio").html(resultadoPeso);
+
+				}
+			}
+
+			sumaTotalCompra()
+			
+		})	
+
+})	
+
+/*=============================================
+/*=============================================
+/*=============================================
+/*=============================================
+/*=============================================
+SUMA TOTAL DE LA COMPRA
+=============================================*/
+function sumaTotalCompra(){
+
+	var sumaTotalTasas = Number($(".valorSubtotal").html())+ 
+	                     Number($(".valorTotalEnvio").html())+ 
+	                     Number($(".valorTotalImpuesto").html());
+
+
+	$(".valorTotalCompra").html(sumaTotalTasas.toFixed(1));
+	localStorage.setItem("total",hex_md5($(".valorTotalCompra").html()));
+
+}
+
+/*=============================================
+/*=============================================
+/*=============================================
+/*=============================================
+/*=============================================
+BOTÓN PAGAR PAYPAL
+=============================================*/
+
+$(".btnPagar").click(function(){
+
+	if($("#seleccionarPais").val() == ""){
+
+		$(".btnPagar").after('<div class="alert alert-warning">No ha seleccionado el país de envío</div>');
+
+		return;
+
+	}
+
+	console.log("PAGAR");
+})	
 
